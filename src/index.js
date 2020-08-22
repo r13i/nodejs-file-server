a14d6c3ef6e5e94abee2601774ab3c23e09532bf
Redouane Achouri <achouri.a.r@gmail.com>
2020-08-22 14:33:06 +0200

    Added initial index.js file.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+
+const methods = Object.create(null);
+const port = process.env.PORT || 8000;
+
+createServer((request, response) => {
+    let handler = methods[request.method] || notAllowed;
+    handler(request)
+        .catch(error => {
+            if (error.status != null) return error
+            else return { body: String(error), status: 500 };
+        })
+        .then(({ body, status = 200, type = 'text/plain'}) => {
+            response.writeHead(status, { "Content-Type": type });
+            if (body && body.pipe) body.pipe(response);
+            else response.end(body);
+        });
+}).listen(port);
+
+console.log(`File server listening on port ${port}`);
+
+
+async function notAllowed(request) {
+    return { status: 405, body: `Method ${request.method} not allowed.`};
+}
